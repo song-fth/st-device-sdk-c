3fa054dcffb04e49008df29014feec07ff90dc13
zihan19-song <zihan19.song@samsung.com>
2024-12-10 14:09:59 +0800

    [bl602 porting] Adapt system APIs 修改了const char *iot_bsp_get_bsp_version_string()接口

--- src/port/bsp/bl602/iot_bsp_system_bl602.c
+++ src/port/bsp/bl602/iot_bsp_system_bl602.c
@@ -25,34 +25,39 @@
 #include "iot_bsp_system.h"
 #include "iot_debug.h"
 
-const char* iot_bsp_get_bsp_name()
+#define BL_SDK_VER release_bl_iot_sdk_1.6.40 - 40 - gfb91fe21a - dirty
+
+const char *iot_bsp_get_bsp_name()
 {
-       return "bl602";
+	return "bl602";
 }
 
-const char* iot_bsp_get_bsp_version_string()
+const char *iot_bsp_get_bsp_version_string()
 {
-       return bl_get_idf_version();
+	setenv("CONFIG_CHIP_NAME", "BL602", 1);
+	getenv("BL_SDK_VER");
+	return BL_SDK_VER;
 }
-
 
 void iot_bl_system_reboot()
 {
-    // Disable scheduler on this core.
-    vTaskSuspendAll();
-	hal_sys_reset();	
+	// Disable scheduler on this core.
+	vTaskSuspendAll();
+	hal_sys_reset();
 }
 
 void iot_bl_system_poweroff()
 {
-	iot_bl_system_reboot();// no poweroff feature.
+	iot_bl_system_reboot(); // no poweroff feature.
 }
 
-iot_error_t iot_bl_system_get_time_in_sec(char* buf, unsigned int buf_len)
+iot_error_t iot_bl_system_get_time_in_sec(char *buf, unsigned int buf_len)
 {
 	IOT_WARN_CHECK(buf == NULL, IOT_ERROR_INVALID_ARGS, "buffer for time is NULL");
 
-	struct timeval tv = {0,};
+	struct timeval tv = {
+		0,
+	};
 
 	gettimeofday(&tv, NULL);
 	snprintf(buf, buf_len, "%lld", tv.tv_sec);
@@ -60,11 +65,13 @@ iot_error_t iot_bl_system_get_time_in_sec(char* buf, unsigned int buf_len)
 	return IOT_ERROR_NONE;
 }
 
-iot_error_t iot_bl_system_set_time_in_sec(const char* time_in_sec)
+iot_error_t iot_bl_system_set_time_in_sec(const char *time_in_sec)
 {
 	IOT_WARN_CHECK(time_in_sec == NULL, IOT_ERROR_INVALID_ARGS, "time data is NULL");
 
-	struct timeval tv = {0,};
+	struct timeval tv = {
+		0,
+	};
 
 	sscanf(time_in_sec, "%lld", &tv.tv_sec);
 	settimeofday(&tv, NULL);
