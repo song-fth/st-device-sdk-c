0bc7e5b7895cbd8edc094558dc0a07bb73df346f
zihan19-song <zihan19.song@samsung.com>
2024-12-11 16:38:16 +0800

    [bl602 porting] Adapt system APIs 修改并测试了本地时间的接口

--- src/port/bsp/bl602/iot_bsp_system_bl602.c
+++ src/port/bsp/bl602/iot_bsp_system_bl602.c
@@ -23,6 +23,7 @@
 #include "hal_sys.h"
 #include "iot_bsp_system.h"
 #include "iot_debug.h"
+#include <hosal_rtc.h>
 
 const char *iot_bsp_get_bsp_name()
 {
@@ -48,16 +49,44 @@ void iot_bl_system_poweroff()
 	iot_bl_system_reboot(); // no poweroff feature.
 }
 
+void iot_bl_system_get_time_in_sec(char *buf, unsigned int buf_len)
+{
+	/* get rtc time */
+	hosal_rtc_get_time(&rtc, &time_buf);
+	/*if(0 != ret) {
+			IOT_ERROR("rtc get time error\r\n");
+			return IOT_ERROR_INVALID_ARGS;
+	}*/
+
+	snprintf(buf, buf_len, "%02hu-%02hhu-%02hhu-%02hhu-%02hhu-%02hhu", time_buf.year, time_buf.month, time_buf.date, time_buf.hr, time_buf.min, time_buf.sec);
+	// return IOT_ERROR_NONE;
+	// hosal_rtc_finalize(&rtc);
+}
+
 iot_error_t iot_bl_system_get_time_in_sec(char *buf, unsigned int buf_len)
 {
 	IOT_WARN_CHECK(buf == NULL, IOT_ERROR_INVALID_ARGS, "buffer for time is NULL");
 
-	struct timeval tv = {
-		0,
-	};
+	int ret = -1;
+	/*init rtc DEC format*/
+	hosal_rtc_dev_t rtc;
+	rtc.port = 0;
+	rtc.config.format = HOSAL_RTC_FORMAT_DEC;
+	hosal_rtc_init(&rtc);
 
-	gettimeofday(&tv, NULL);
-	snprintf(buf, buf_len, "%lld", tv.tv_sec);
+	hosal_rtc_time_t time_buf;
+	/* clear time buf*/
+	memset(&time_buf, 0, sizeof(hosal_rtc_time_t));
+
+	/* get rtc time */
+	ret = hosal_rtc_get_time(&rtc, &time_buf);
+	if (0 != ret)
+	{
+		IOT_ERROR("rtc get time error\r\n");
+		return IOT_ERROR_INVALID_ARGS;
+	}
+
+	snprintf(buf, buf_len, "%02hu-%02hhu-%02hhu-%02hhu-%02hhu-%02hhu", time_buf.year, time_buf.month, time_buf.date, time_buf.hr, time_buf.min, time_buf.sec);
 
 	return IOT_ERROR_NONE;
 }
@@ -66,12 +95,32 @@ iot_error_t iot_bl_system_set_time_in_sec(const char *time_in_sec)
 {
 	IOT_WARN_CHECK(time_in_sec == NULL, IOT_ERROR_INVALID_ARGS, "time data is NULL");
 
-	struct timeval tv = {
-		0,
-	};
+	int ret = -1;
+	/*init rtc DEC format*/
+	hosal_rtc_dev_t rtc;
+	rtc.port = 0;
+	rtc.config.format = HOSAL_RTC_FORMAT_DEC;
+	hosal_rtc_init(&rtc);
 
-	sscanf(time_in_sec, "%lld", &tv.tv_sec);
-	settimeofday(&tv, NULL);
+	hosal_rtc_time_t time_buf;
+	/* clear time buf*/
+	memset(&time_buf, 0, sizeof(hosal_rtc_time_t));
+
+	if (6 != sscanf(time_in_sec, "%2hu-%2hhu-%2hhu-%2hhu-%2hhu-%2hhu", &time_buf.year, &time_buf.month, &time_buf.date, &time_buf.hr, &time_buf.min, &time_buf.sec))
+	{
+		IOT_ERROR("Parsing failure\r\n");
+	}
+	else
+	{
+		printf("%hu-%hhu-%hhu-%hhu-%hhu-%hhu\r\n", time_buf.year + 2000, time_buf.month, time_buf.date, time_buf.hr, time_buf.min, time_buf.sec);
+	}
+
+	ret = hosal_rtc_set_time(&rtc, &time_buf);
+	if (0 != ret)
+	{
+		IOT_ERROR("rtc get time error\r\n");
+		return IOT_ERROR_INVALID_ARGS;
+	}
 
 	return IOT_ERROR_NONE;
 }
